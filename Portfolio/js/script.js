ccd662428adc041a1980220382179f28446bf929
Roman Savransky'y <91686101+Quadatz@users.noreply.github.com>
2022-09-10 01:41:37 +0200

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.hamburger'), 
+menu = document.querySelector('.menu')
+closeElem = document.querySelector('.menu__close')
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+})
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
+const counters = document.querySelectorAll('.skills__sliders-item_percentage'),
+lines = document.querySelectorAll('.skills__slider_fill')
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
