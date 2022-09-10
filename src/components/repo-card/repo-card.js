946bc46fddb31998edaa627466314f90e97d35ae
Quadatz <quadath@gmail.com>
2022-09-10 03:15:55 +0300

    preview

--- src/components/repo-card/repo-card.js
+++ src/components/repo-card/repo-card.js
@@ -4,8 +4,8 @@ import './repo-card.sass'
 export default function RepoCard({name, link, codeLink}) {
   return (
     <div className="repo-card">
+      <img className="repo-card__img" src={`https://raw.githubusercontent.com/Quadatz/Summary/master/${name}/prev.png`} alt="preview"/>
       <a target="_blank" href={`https://quadatz.github.io/Summary/${name}`}><h2>{name}</h2></a>
-      <img src={`https://quadatz.github.io/Summary/${name}/prev.png`} alt="preview"/>
     </div>
   )
 }
